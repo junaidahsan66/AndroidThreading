ff88c645ae4bfa17f81c16d5ee1fa8e4f487bac7
junaid <ahsan.junaid@senrysa.com>
2021-08-03 14:55:19 +0530

    android thread

---
+++ app/src/main/java/com/aexample/threadingandroid/MainActivity.java
@@ -0,0 +1,60 @@
+package com.aexample.threadingandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ScrollView;
+
+import com.aexample.threadingandroid.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.textView.setText(R.string.lorem);
+
+
+        binding.start.setOnClickListener(v -> {
+            enableProgress(true);
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    enableProgress(false);
+                }
+            };
+
+            Handler handler = new Handler();
+            handler.postDelayed(runnable,4000);
+
+            scrollToEnd();
+        });
+        binding.end.setOnClickListener(v -> {
+            enableProgress(false);
+        });
+    }
+
+    private void scrollToEnd() {
+        binding.scroll.post(new Runnable() {
+            @Override
+            public void run() {
+                binding.scroll.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+    public void enableProgress(Boolean value){
+        if (value){
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.GONE);
+
+        }
+
+    }
+}
