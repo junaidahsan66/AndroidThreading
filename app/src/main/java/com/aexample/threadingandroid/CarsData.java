392ebbf161a63e616df484d5be7d7fb33773e2ac
junaid <ahsan.junaid@senrysa.com>
2021-08-04 13:28:29 +0530

    android thread

---
+++ app/src/main/java/com/aexample/threadingandroid/CarsData.java
@@ -0,0 +1,6 @@
+package com.aexample.threadingandroid;
+
+public class CarsData {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+}
---
+++ app/src/main/java/com/aexample/threadingandroid/DownloadThread.java
@@ -0,0 +1,4 @@
+package com.aexample.threadingandroid;
+
+public class DownloadThread extends Thread {
+}
